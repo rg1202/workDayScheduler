d49868ca23e9dcdcf0b054d75550b41101798d5c
Rob Gould <rgould.midwest@gmail.com>
2023-10-22 11:43:38 -0500

    add test.html to work alongside index for the time being; modified css; added displaying current date and time on page header- start coding logic and formatting structure

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 ## Your Task
 
-Create a simple calendar application that allows a user to save events for each hour of a typical working day (9am&ndash;5pm) by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+Create a simple calendar application that allows a user to save events for each hour of a typical working day (9am-5pm) by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
 
 You'll need to use the [Day.js](https://day.js.org/en/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Day.js in the browser.
 
 ## User Story
 
-```md
+```
 AS AN employee with a busy schedule
 I WANT to add important events to a daily planner
 SO THAT I can manage my time effectively
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +20,54 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  $(document).ready(function() {
+    const currentDayElement = $('#currentDay');
+    const currentTime = dayjs().format('dddd, MMMM D, YYYY h:mm A');
+    currentDayElement.text(currentTime);
+    const workingHours = 8; // Number of working hours
+    const startHour = 9;    // Start at 9 AM
+    const $calendar = $('#calendar');
+    const currentDay = dayjs(); // Get the current day
+
+    // Generate time slots
+    for (let i = 0; i < workingHours; i++) {
+        const hour = startHour + i;
+        const timeSlot = dayjs().hour(hour).minute(0);
+        const $row = $('<div class="row">');
+        const $timeColumn = $('<div class="hour col-md-2">').text(timeSlot.format('h A'));
+        const $eventColumn = $('<div class="event col-md-10">');
+        const eventKey = `event-${hour}`;
+
+        // Retrieve stored events from local storage
+        const storedEvent = localStorage.getItem(eventKey);
+        if (storedEvent) {
+            $eventColumn.text(storedEvent);
+        }
+
+        // Create an input for new events
+        const $eventInput = $('<input type="text" class="form-control">');
+        $eventInput.val(storedEvent);
+
+        // Create a button to save the event
+        const $saveButton = $('<button class="btn btn-primary col-md-2">Save</button>');
+        $saveButton.on('click', function() {
+            const newEvent = $eventInput.val();
+            localStorage.setItem(eventKey, newEvent);
+        });
+
+        // Determine if the hour is in the past, present, or future
+        if (timeSlot.isBefore(currentDay, 'hour')) {
+            $row.addClass('past');
+        } else if (timeSlot.isSame(currentDay, 'hour')) {
+            $row.addClass('present');
+        } else {
+            $row.addClass('future');
+        }
+
+        $eventColumn.append($eventInput, $saveButton);
+        $row.append($timeColumn, $eventColumn);
+        $calendar.append($row);
+    };
 });
+
+
